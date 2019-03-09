cbe3d2d16f0dd19f58446329f299792f408422e8
Johannes Opper <xijo@pm.me>
2019-03-09 18:56:49 +0100

    Work work

--- src/App.jsx
+++ src/App.jsx
@@ -1,212 +1,40 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react'
 import Helmet from 'react-helmet'
 
-import ScrollToTop from './components/ScrollToTop'
 import Meta from './components/Meta'
-import Home from './views/Home'
-import About from './views/About'
-import Blog from './views/Blog'
-import SinglePost from './views/SinglePost'
-import Contact from './views/Contact'
-import NoMatch from './views/NoMatch'
 import Week from './components/Week'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
-import ServiceWorkerNotifications from './components/ServiceWorkerNotifications'
 import data from './data.json'
-import { slugify } from './util/url'
-import { documentHasTerm, getCollectionTerms } from './util/collection'
-
-import {getISOWeek} from 'date-fns'
-
-import {Calendar} from 'react-feather'
 
 import './css/custom.css'
 
-// import formatRelative from 'date-fns/format_relative'
-// import subDays from 'date-fns/sub_days'
-
-const RouteWithMeta = ({ component: Component, ...props }) => (
-  <Route
-    {...props}
-    render={routeProps => (
-      <Fragment>
-        <Meta {...props} />
-        <Component {...routeProps} {...props} />
-      </Fragment>
-    )}
-  />
-)
-
-class App extends Component {
+class App extends React.Component {
   state = {
     data
   }
 
-  getDocument = (collection, name) =>
-    this.state.data[collection] &&
-    this.state.data[collection].filter(page => page.name === name)[0]
-
-  getDocuments = collection => this.state.data[collection] || []
-
   render () {
-    const globalSettings = this.getDocument('settings', 'global')
-    const {
-      siteTitle,
-      siteUrl,
-      siteDescription,
-      socialMediaCard,
-      headerScripts
-    } = globalSettings
+    // const globalSettings = this.getDocument('settings', 'global')
+    // const {
+    //   siteTitle,
+    //   siteUrl,
+    //   socialMediaCard,
+    //   headerScripts
+    // } = globalSettings
 
-    const posts = this.getDocuments('posts').filter(
-      post => post.status !== 'Draft'
-    )
-    const categoriesFromPosts = getCollectionTerms(posts, 'categories')
-    const postCategories = this.getDocuments('postCategories').filter(
-      category => categoriesFromPosts.indexOf(category.name.toLowerCase()) >= 0
-    )
-
-    console.log(getISOWeek(new Date(2018, 11, 31)))
-    console.log(getISOWeek(new Date(2005, 0, 2)))
-    // console.log(formatRelative(subDays(new Date(), 3), new Date(), { locale: 'de' }))
+    const calendarweeks = this.state.data.calendarweeks.filter(cw => cw.visible).map((cw, index) => { return <Week key={index} {...cw} /> })
 
     return <div className="w-full max-w-screen-xl mx-auto px-6">
       <div className="lg:flex -mx-6">
         <div className="mx-auto min-h-screen w-full lg:static lg:max-h-full lg:overflow-visible lg:w-3/4 xl:w-4/5 ">
-          {/* <Calendar size={40}/> */}
-
-
           <h1 className='text-3xl mt-10 mb-5 font-serif font-normal text-center'>
             <span className='inline-block rounded border border-black font-bold border-t-8 py-2 px-1 mr-1 border-blue-dark'>Mo</span>
             ntagsbild
           </h1>
 
-          <Week cw={3} year={2019} />
-          <Week cw={2} year={2019} />
-          <Week cw={1} year={2019} />
-          <Week cw={52} year={2018} />
+          {calendarweeks}
         </div>
       </div>
     </div>
-
-    return (
-      <Router>
-        <div className='React-Wrap'>
-          <ScrollToTop />
-          <ServiceWorkerNotifications reloadOnUpdate />
-          <Helmet
-            defaultTitle={siteTitle}
-            titleTemplate={`${siteTitle} | %s`}
-          />
-          <Meta
-            headerScripts={headerScripts}
-            absoluteImageUrl={
-              socialMediaCard &&
-              socialMediaCard.image &&
-              siteUrl + socialMediaCard.image
-            }
-            twitterCreatorAccount={
-              socialMediaCard && socialMediaCard.twitterCreatorAccount
-            }
-            twitterSiteAccount={
-              socialMediaCard && socialMediaCard.twitterSiteAccount
-            }
-          />
-
-          <Nav />
-
-          <div className="container mx-auto">
-            <div className="flex items-center py-2 mx-24">
-              <div className="w-1/2">
-                <div className="flex items-center">
-                </div>
-              </div>
-
-            </div>
-            <div className="flex mb-4">
-              <div className="w-full bg-grey h-12"></div>
-            </div>
-
-          </div>
-
-          <div className="flex mb-4">
-            <div className="w-full bg-grey h-12"></div>
-          </div>
-
-          {/* <Switch>
-            <RouteWithMeta
-              path='/'
-              exact
-              component={Home}
-              description={siteDescription}
-              fields={this.getDocument('pages', 'home')}
-            />
-            <RouteWithMeta
-              path='/about/'
-              exact
-              component={About}
-              fields={this.getDocument('pages', 'about')}
-            />
-            <RouteWithMeta
-              path='/contact/'
-              exact
-              component={Contact}
-              fields={this.getDocument('pages', 'contact')}
-              siteTitle={siteTitle}
-            />
-            <RouteWithMeta
-              path='/blog/'
-              exact
-              component={Blog}
-              fields={this.getDocument('pages', 'blog')}
-              posts={posts}
-              postCategories={postCategories}
-            />
-
-            {posts.map((post, index) => {
-              const path = slugify(`/blog/${post.title}`)
-              const nextPost = posts[index - 1]
-              const prevPost = posts[index + 1]
-              return (
-                <RouteWithMeta
-                  key={path}
-                  path={path}
-                  exact
-                  component={SinglePost}
-                  fields={post}
-                  nextPostURL={nextPost && slugify(`/blog/${nextPost.title}/`)}
-                  prevPostURL={prevPost && slugify(`/blog/${prevPost.title}/`)}
-                />
-              )
-            })}
-
-            {postCategories.map(postCategory => {
-              const slug = slugify(postCategory.title)
-              const path = slugify(`/blog/category/${slug}`)
-              const categoryPosts = posts.filter(post =>
-                documentHasTerm(post, 'categories', slug)
-              )
-              return (
-                <RouteWithMeta
-                  key={path}
-                  path={path}
-                  exact
-                  component={Blog}
-                  fields={this.getDocument('pages', 'blog')}
-                  posts={categoryPosts}
-                  postCategories={postCategories}
-                />
-              )
-            })}
-
-            <Route render={() => <NoMatch siteUrl={siteUrl} />} />
-          </Switch> */}
-          <Footer />
-        </div>
-      </Router>
-    )
   }
 }
 
--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,47 +1,79 @@
 import React from 'react'
+import _groupBy from 'lodash/groupBy'
+
+import Modal from './Modal'
 
 export default class Week extends React.Component {
-  state = { expanded: true }
+  state = { modal: false }
 
   render() {
-    return <div className="relative max-w-sm overflow-hiden shadow-lg mb-10 mx-auto border-4 border-white rounded">
-      <img className="w-full rounded cursor-pointer" src="https://tailwindcss.com/img/card-top.jpg" onClick={() => this.setState({expanded: !this.state.expanded})} alt="Sunset in the mountains" />
+    const [year, cw] = this.props.title.split("-")
+    const groupedPhotos = _groupBy(this.props.photos, "day")
 
-      <div className='absolute block font-serif rounded border border-black border-t-8 p-2 pb-1 mr-1 pin-t border-blue-dark text-center' style={{right: -70, zIndex: -10}}>
-        <div>{this.props.year}</div>
-        <div className='text-2xl font-bold'>{this.props.cw}</div>
+    return <div className="relative max-w-sm overflow-hiden shadow-lg mb-10 mx-auto border-4 border-white rounded">
+      <img className="w-full rounded cursor-pointer" src={this.props.image} onClick={() => this.setState({modal: true})} alt={this.props.name} />
+
+      {this.state.modal && <Modal onClose={() => this.setState({modal: false})}>
+        <img className="w-full rounded cursor-pointer" src={this.props.image} alt={this.props.name} />
+      </Modal>}
+
+      <div className='absolute pin-t' style={{right: -90, zIndex: -10}}>
+        <Calendar top={year} bottom={`KW ${cw}`} />
       </div>
 
-      { this.state.expanded && <div className="p-3">
-        <div className="font-sans font-normal text-xl mb-2">The Coldest Sunset</div>
+      <div className="p-3">
+        <div className="font-sans font-normal text-xl mb-2">{this.props.name}</div>
         <p className="text-grey-darker text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          {this.props.description}
         </p>
-      </div> }
-      { this.state.expanded && <div className="px-6 py-4 flex items-center justify-between">
-        <Day abbr='Mo' />
-        <Day abbr='Di' />
-        <Day abbr='Mi' images={['http://placeowl.com/40/30']} />
-        <Day abbr='Do' />
-        <Day abbr='Fr' />
-        <Day abbr='Sa' />
-        <Day abbr='So' />
-      </div> }
+      </div>
+
+      <div className="px-6 py-4 flex items-center justify-between">
+        <Day abbr='Mo' cw={cw} photos={groupedPhotos.Mo} />
+        <Day abbr='Di' cw={cw} photos={groupedPhotos.Di} />
+        <Day abbr='Mi' cw={cw} photos={groupedPhotos.Mi} />
+        <Day abbr='Do' cw={cw} photos={groupedPhotos.Do} />
+        <Day abbr='Fr' cw={cw} photos={groupedPhotos.Fr} />
+        <Day abbr='Sa' cw={cw} photos={groupedPhotos.Sa} />
+        <Day abbr='So' cw={cw} photos={groupedPhotos.So} />
+      </div>
     </div>
   }
 }
 
 class Day extends React.Component {
+  state = { modal: false }
+
   render() {
-    if (this.props.images) {
+    if (this.props.photos) {
       return <div className="mr-2 bg-lightgrey">
-        <div className='inline-block relative z-10 stack cursor-pointer'>
-          <img className="shadow bg-white p-1" alt="" src={this.props.images[0]} />
+        <div className='inline-block relative z-10 stack cursor-pointer' onClick={() => this.setState({modal: true})}>
+          <img className="shadow bg-white w-12 p-1" alt="" src={this.props.photos[0].image} />
         </div>
+        {this.state.modal && <Modal padded onClose={() => this.setState({modal: false})}>
+          <img className="w-full rounded" alt="" src={this.props.photos[0].image} />
+          <div className="flex mt-2">
+            <Calendar top={`KW ${this.props.cw}`} bottom={this.props.abbr} />
+            <div className="flex flex-col ml-2">
+              <p className="text-grey-darker text-base">
+                {this.props.photos[0].description}
+              </p>
+            </div>
+          </div>
+        </Modal>}
       </div>
     }
     return <div className="mr-2 bg-lightgrey">
       <h3 className='text-xl text-grey font-serif font-hairline cursor-default'>{this.props.abbr}</h3>
+    </div>
+  }
+}
+
+class Calendar extends React.Component {
+  render() {
+    return <div className='inline-block font-serif rounded border border-grey-darker border-t-8 p-2 pb-1 text-center'>
+      <div>{this.props.top}</div>
+      <div className='text-xl font-bold'>{this.props.bottom}</div>
     </div>
   }
 }
